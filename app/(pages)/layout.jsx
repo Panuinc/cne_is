b84a3dfb6752ef940baf994f54a6f9dda43ce290
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-18 22:10:06 +0700

    update

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -294,8 +294,15 @@ export default function PagesLayout({ children }) {
               className="flex items-center justify-center w-full min-h-[72px] p-2 gap-2 border-t-2 border-white"
               onClick={() => setCollapsed(!collapsed)}
             >
-              <div className="flex items-center justify-center w-full h-full p-2 gap-2 hover:bg-white">
-                <Left />
+              <div className="flex items-center justify-center w-full h-full p-2 gap-2 hover:bg-white cursor-pointer">
+                <div className="flex items-center justify-center h-full p-2 gap-2">
+                  <Left />
+                </div>
+                {!collapsed && (
+                  <div className="flex items-center justify-start w-full h-full p-2 gap-2">
+                    ย่อแถบ
+                  </div>
+                )}
               </div>
             </div>
           </div>
