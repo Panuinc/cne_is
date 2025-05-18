6245bb52811c1e6462954136b4c6c3c5d99a4cf2
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-18 16:36:43 +0700

    update

--- components/ui/home/UIHome.jsx
+++ components/ui/home/UIHome.jsx
@@ -1,13 +1,10 @@
+import UIHeader from "@/components/other/UIHeader";
 import React from "react";
 
 export default function UIHome() {
   return (
     <>
-      <div className="flex items-center justify-center w-full p-2 gap-2 border-b-2 border-default">
-        <div className="flex items-center justify-start w-full h-full p-2 gap-2">
-          Header
-        </div>
-      </div>
+      <UIHeader Header="Home" />
       <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 bg-white overflow-auto border-2 border-dark">
         <div className="flex items-center justify-center w-full p-2 gap-2 border-2 border-dark">
           1
