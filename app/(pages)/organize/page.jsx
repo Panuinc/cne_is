33f7725e899682e2c3a7dd60a55879a50296054c
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-19 11:24:20 +0700

    update

---
+++ app/(pages)/organize/page.jsx
@@ -0,0 +1,10 @@
+import UIOrganize from '@/components/ui/organize/UIOrganize'
+import React from 'react'
+
+export default function Organize() {
+  return (
+    <>
+      <UIOrganize />
+    </>
+  )
+}
