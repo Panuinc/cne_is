6183cc500e5962b2049bee7d34f7af23e89d7589
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-20 17:04:48 +0700

    update

--- UIDemo.jsx
+++ UIDemo.jsx
@@ -1,15 +1,13 @@
 import UIHeader from "@/components/other/UIHeader";
 import React from "react";
 
-export default function UIPosJobDes({ header }) {
+export default function UIAccount() {
   return (
     <>
-      <UIHeader Header={header} />
-      <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark bg-white overflow-auto">
-        <div className="flex flex-col xl:flex-row items-center justify-start w-full p-2 gap-2 border-2 border-dark">
-          <div className="flex items-center justify-center h-full p-4 gap-2 bg-primary/75 border-2 border-primary text-white rounded-lg">
-            ข้อมูล ใบกำหนดลักษณะงาน
-          </div>
+      <UIHeader Header="โปรไฟล์" />
+      <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 bg-white overflow-auto border-2 border-dark">
+        <div className="flex items-center justify-center w-full p-2 gap-2 border-2 border-dark">
+          1
         </div>
       </div>
     </>
--- components/ui/home/UIHome.jsx
+++ components/ui/home/UIHome.jsx
@@ -6,25 +6,25 @@ export default function UIHome() {
     <>
       <UIHeader Header="หน้าหลัก" />
       <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 bg-white overflow-auto">
-        <div className="flex flex-row items-center justify-center w-full p-2 gap-2">
-          <div className="flex items-center justify-center w-6/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full p-2 gap-2">
+          <div className="flex items-center justify-center w-full xl:w-6/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 1
           </div>
-           <div className="flex items-center justify-center w-4/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+          <div className="flex items-center justify-center w-full xl:w-4/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 2
           </div>
-           <div className="flex items-center justify-center w-2/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+          <div className="flex items-center justify-center w-full xl:w-2/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 3
           </div>
         </div>
-        <div className="flex flex-row items-center justify-center w-full p-2 gap-2">
-          <div className="flex items-center justify-center w-2/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full p-2 gap-2">
+          <div className="flex items-center justify-center w-full xl:w-2/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 4
           </div>
-           <div className="flex items-center justify-center w-6/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+          <div className="flex items-center justify-center w-full xl:w-6/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 5
           </div>
-           <div className="flex items-center justify-center w-4/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
+          <div className="flex items-center justify-center w-full xl:w-4/12 min-h-64 p-2 gap-2 border-2 border-default bg-white rounded-lg shadow-md">
             Dashboard 6
           </div>
         </div>
