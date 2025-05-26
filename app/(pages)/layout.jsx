0f2941fcb1af144d9cba74e69d8db2518dae23ea
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-26 13:31:19 +0700

    update

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -319,9 +319,9 @@ export default function PagesLayout({ children }) {
             ${collapsed ? "xl:w-[30%]" : "xl:w-[40%]"}
             w-[30%]`}
           >
-            <div className="flex items-center justify-center w-full min-h-[72px] gap-2">
+            <div className="flex items-center justify-center w-full min-h-[72px] gap-2 border-b-1 border-default font-[600]">
               <div className="flex items-center justify-center w-full h-full p-2 gap-2">
-                {""}
+                {session?.user?.nameTH || session?.user?.nameEN || "ชื่อผู้ใช้"}
               </div>
             </div>
             <div className="flex flex-col items-center justify-start w-full h-full gap-2 overflow-auto">
