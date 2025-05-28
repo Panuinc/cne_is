ca8b15e2b7af467029efb5388e45da471094becb
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-05-28 10:59:41 +0700

    update

--- app/api/hr/perReq/perReqPDF/perReqPDFService.js
+++ app/api/hr/perReq/perReqPDF/perReqPDFService.js
@@ -110,21 +110,7 @@ export class PerReqService {
         },
         PerReqDivisionId: { select: { divisionName: true } },
         PerReqDepartmentId: { select: { departmentName: true } },
-        PerReqPositionId: {
-          select: {
-            positionNameTH: true,
-            positionIdPosJobDes: {
-              select: {
-                posJobDesAge: true,
-                posJobDesSex: true,
-                posJobDesEducations: true,
-                posJobDesSkill: true,
-                posJobDesExperience: true,
-                posJobDesResponsibility: true,
-              },
-            },
-          },
-        },
+        PerReqPositionId: { select: { positionNameTH: true } },
       },
     });
   }
