c79b825b688c9e506c776a16d667c2e5c01f0a08
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-06-17 16:27:41 +0700

    update

--- components/ui/hr/recruit/components/UIRecruitStep5.jsx
+++ components/ui/hr/recruit/components/UIRecruitStep5.jsx
@@ -1,9 +1,175 @@
-import React from 'react'
+"use client";
 
-export default function UIRecruitStep5() {
+import SignatureCanvas from "react-signature-canvas";
+import { useRef } from "react";
+import { Button } from "@heroui/react";
+import { MinusCircle, PlusCircle } from "lucide-react";
+import {
+  renderInputField,
+  renderTextAreaField,
+  renderRadioGroupField,
+  renderSelectField,
+} from "@/components/ui/hr/recruit/components/UIRenderField";
+
+export default function UIRecruitStep5({
+  formData,
+  handleInputChange,
+  errors,
+}) {
+  const sigCanvasRef = useRef();
+
+  const clearSignature = () => {
+    sigCanvasRef.current.clear();
+    handleInputChange("recruitDetail.recruitSignature")("");
+  };
+
+  const saveSignature = () => {
+    const signatureData = sigCanvasRef.current
+      .getTrimmedCanvas()
+      .toDataURL("image/png");
+    handleInputChange("recruitDetail.recruitSignature")(signatureData);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-4 border-danger">
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          {renderTextAreaField({
+            labelTH: "กรุณาแนะนำตัวท่านเอง เพื่อให้บริษัทรู้จักท่านดีขึ้น",
+            labelEN:
+              "Please provide any further information about yourself which will allow our company to know you better",
+            name: "recruitDetail.recruitDetailSelfIntro",
+            type: "text",
+            value: formData?.recruitDetail?.recruitDetailSelfIntro || "",
+            onChange: handleInputChange("recruitDetail.recruitDetailSelfIntro"),
+            error: errors?.["recruitDetail.recruitDetailSelfIntro"],
+          })}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-4 border-danger">
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+            ข้าพเจ้าขอรับรองว่า ข้อความทั้งหมดในใบสมัครนี้เป็นความจริงทุกประการ
+            หากบริษัทฯ จ้างเข้าทำงานแล้วปรากฏว่าข้อความในใบสมัครงาน
+            เอกสารที่นำมาแสดง หรือรายละเอียดที่ให้ไว้ไม่เป็นความจริง บริษัทฯ
+            มีสิทธิ์ที่จะเลิกจ้างข้าพเจ้าได้โดยไม่ต้องจ่ายเงินชดเชยหรือค่าเสียหายใด
+            ๆ ทั้งสิ้น
+            <br />I hereby certify that all the information provided in this
+            application is true and correct. If the company later discovers that
+            any information, documents, or details provided are false, the
+            company reserves the right to terminate my employment without any
+            compensation or damages.
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning text-md font-[600]">
+          <SignatureCanvas
+            ref={sigCanvasRef}
+            penColor="black"
+            canvasProps={{
+              width: 500,
+              height: 200,
+              className: "border-b-2 border-dark/25",
+            }}
+          />
+          ลายมือชื่อผู้สมัคร / Applicant’s signature
+          <div className="flex gap-2 mt-2">
+            <Button
+              color="primary"
+              size="md"
+              radius="full"
+              className="flex items-center justify-center h-full px-8 py-4 gap-2 border-2 border-dark"
+              onPress={saveSignature}
+            >
+              บันทึกลายเซ็น
+            </Button>
+            <Button
+              color="danger"
+              size="md"
+              radius="full"
+              className="flex items-center justify-center h-full px-8 py-4 gap-2 border-2 border-dark"
+              onPress={clearSignature}
+            >
+              ล้างลายเซ็น
+            </Button>
+          </div>
+          {errors?.["recruitDetail.recruitDetailSignatureImage"] && (
+            <div className="text-red-500 text-sm">
+              {errors["recruitDetail.recruitDetailSignatureImage"]}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-4 border-danger">
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+            เอกสารที่แนบพร้อมใบสมัคร
+            <br />
+            Enclosed Documents
+          </div>
+        </div>
+
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          {renderInputField({
+            labelTH: "สำเนาบัตรประชาชน",
+            labelEN: "Identification Card",
+            name: "recruitDetail.recruitDetailAttachIdCard",
+            type: "file",
+            value: formData?.recruitDetail?.recruitDetailAttachIdCard || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitDetailAttachIdCard"
+            ),
+            error: errors?.["recruitDetail.recruitDetailAttachIdCard"],
+          })}
+          {renderInputField({
+            labelTH: "สำเนาทะเบียนบ้าน",
+            labelEN: "House Registration",
+            name: "recruitDetail.recruitDetailAttachHouseReg",
+            type: "file",
+            value: formData?.recruitDetail?.recruitDetailAttachHouseReg || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitDetailAttachHouseReg"
+            ),
+            error: errors?.["recruitDetail.recruitDetailAttachHouseReg"],
+          })}
+          {renderInputField({
+            labelTH: "หลักฐานการศึกษา",
+            labelEN: "Transcript",
+            name: "recruitDetail.recruitDetailAttachEducation",
+            type: "file",
+            value: formData?.recruitDetail?.recruitDetailAttachEducation || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitDetailAttachEducation"
+            ),
+            error: errors?.["recruitDetail.recruitDetailAttachEducation"],
+          })}
+          {renderInputField({
+            labelTH: "ใบรับรองแพทย์",
+            labelEN: "Medical Certificate",
+            name: "recruitDetail.recruitDetailAttachMedicalCert",
+            type: "file",
+            value:
+              formData?.recruitDetail?.recruitDetailAttachMedicalCert || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitDetailAttachMedicalCert"
+            ),
+            error: errors?.["recruitDetail.recruitDetailAttachMedicalCert"],
+          })}
+          {renderInputField({
+            labelTH: "หลักฐานการเกณฑ์ทหาร",
+            labelEN: "Military Certificate",
+            name: "recruitDetail.recruitDetailAttachMilitaryDoc",
+            type: "file",
+            value:
+              formData?.recruitDetail?.recruitDetailAttachMilitaryDoc || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitDetailAttachMilitaryDoc"
+            ),
+            error: errors?.["recruitDetail.recruitDetailAttachMilitaryDoc"],
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
--- components/ui/hr/recruit/components/UIRecruitStep6.jsx
+++ components/ui/hr/recruit/components/UIRecruitStep6.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+"use client";
 
-export default function UIRecruitStep6() {
+import SignatureCanvas from "react-signature-canvas";
+import { useRef } from "react";
+import { Button } from "@heroui/react";
+import { MinusCircle, PlusCircle } from "lucide-react";
+import {
+  renderInputField,
+  renderTextAreaField,
+  renderRadioGroupField,
+  renderSelectField,
+} from "@/components/ui/hr/recruit/components/UIRenderField";
+
+export default function UIRecruitStep6({
+  formData,
+  handleInputChange,
+  errors,
+}) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-4 border-danger">
+        <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+            ความยินยอมเกี่ยวกับการประมวลผลข้อมูลส่วนบุคคลทั่วไป
+            <br />
+            Consent to the processing of general personal data
+          </div>
+          <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+            บริษัท ชาญนครวิศวกรรม จำกัด และบริษัทย่อย (รวมเรียกว่า “บริษัท”)
+            มีความจำเป็นที่จะต้องเก็บ รวบรวม ใช้ รวมทั้งเปิดเผย
+            (ซึ่งต่อไปนี้เรียกรวมกันว่า “ประมวลผล”)
+            ข้อมูลส่วนบุคคลของท่านเพื่อใช้ประกอบกระบวนการพิจารณาใบสมัครงานของท่าน
+            เช่น การสรรหาบุคลากร การตรวจสอบ
+            การพิจารณาคุณสมบัติและความเหมาะสมในตำแหน่งงานที่ท่านได้สมัครไว้กับบริษัทหรือตำแหน่งงานอื่นใดที่บริษัทเห็นว่ามีความเหมาะสมกับท่าน
+            <br />
+            CHAN NAKORN ENGINEERING CO., LTD. and its subsidiaries (collectively
+            referred to as the “Company”), have to collect, use, and disclose
+            (collectively “Process”) your personal data as a part of the process
+            for considering your job application such as recruitment,
+            verification, and evaluation of your qualifications and suitability
+            for the position you applied for, or any position which the Company
+            deems appropriate for you.
+          </div>
+
+          <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+            นอกจากนี้ ในกรณีที่ท่านได้รับการพิจารณาให้เป็นพนักงานของบริษัทแล้ว
+            บริษัทจะนำข้อมูลส่วนบุคคลที่ท่านได้ให้ไว้กับบริษัทไปใช้ประโยชน์ในการบริหารงานทรัพยากรบุคคล
+            สิทธิประโยชน์ และสวัสดิการซึ่งเกี่ยวกับตัวท่านตามหลักเกณฑ์ของบริษัท
+            และ/หรือหลักเกณฑ์ตามกฎหมายที่เกี่ยวข้อง
+            <br />
+            Moreover, in the event that you are accepted to be an employee of
+            the Company, the Company will use your personal data provided by you
+            for the purposes of managing human resources, benefits, and welfare
+            related to you in accordance with the Company’s regulations and/or
+            applicable laws.
+          </div>
+        </div>
+
+        <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+          ความยินยอมเกี่ยวกับข้อมูลส่วนบุคคลทั่วไป
+          <br />
+          Consent to the processing of general personal data
+        </div>
+
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          {renderRadioGroupField({
+            labelTH: "ข้าพเข้า",
+            labelEN: "I",
+            name: "recruitDetail.recruitConsentGeneral",
+            value: formData?.recruitDetail?.recruitConsentGeneral || "",
+            onChange: handleInputChange("recruitDetail.recruitConsentGeneral"),
+            error: errors?.["recruitDetail.recruitConsentGeneral"],
+            options: [
+              { labelTH: "ยินยอม", labelEN: "Yes", value: "Yes" },
+              { labelTH: "ไม่ยินยอม", labelEN: "No", value: "No" },
+            ],
+          })}
+        </div>
+
+        <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+          ให้บริษัทประมวลผลข้อมูลส่วนบุคคลของข้าพเจ้าเพื่อพิจารณาตำแหน่งงานที่เหมาะสม
+          และแจ้งเตือนเมื่อมีการประกาศรับสมัครงานใหม่
+          <br />I consent to the Company processing my personal data for the
+          purpose of considering suitable job positions and notifying me when
+          new job openings are announced.
+        </div>
+
+        <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+          ความยินยอมเกี่ยวกับการประมวลผลข้อมูลส่วนบุคคลที่มีความอ่อนไหว
+          <br />
+          Consent to the processing of sensitive personal data
+        </div>
+
+        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-4 border-warning">
+          {renderRadioGroupField({
+            labelTH: "ข้าพเข้า",
+            labelEN: "I",
+            name: "recruitDetail.recruitConsentSensitive",
+            value: formData?.recruitDetail?.recruitConsentSensitive || "",
+            onChange: handleInputChange(
+              "recruitDetail.recruitConsentSensitive"
+            ),
+            error: errors?.["recruitDetail.recruitConsentSensitive"],
+            options: [
+              { labelTH: "ยินยอม", labelEN: "Yes", value: "Yes" },
+              { labelTH: "ไม่ยินยอม", labelEN: "No", value: "No" },
+            ],
+          })}
+        </div>
+
+        <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark text-md font-[600]">
+          ในการแสดงเจตนาของข้าพเจ้า
+          ข้าพเจ้าเข้าใจและรับทราบถึงรายละเอียดตามที่กำหนดไว้ในประกาศคุ้มครองข้อมูลส่วนบุคคลสำหรับผู้สมัครเข้าร่วมงาน
+          ผู้สมัครเข้าฝึกงาน และผู้ฝึกงาน ซึ่งแนบมากับใบสมัครงานฉบับนี้
+          <br />I hereby acknowledge and understand the details specified in the
+          Personal Data Protection Notice for Job Applicants, Internship
+          Applicants, and Interns attached to this application.
+        </div>
+      </div>
+    </>
+  );
 }
